bdb34b7236408a782c12ccaf9e2106159c16191d
Jacob McGuire <dev.jmcguire@gmail.com>
2021-09-23 15:37:16 -0400

    minor fixes for header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -8,14 +8,16 @@ import "./Header.scss";
 const Header: React.FC<HeaderProps> = ({ links, justify, theme, fontSize }) => (
     <header data-testid="Header" className={`${theme ? theme : 'light'}-bg`}>
         <nav style={{ justifyContent: justify }} data-testid="header-nav" className="jm-header-nav">
-        {links.map((link) => <a 
+        {links.map((link) => <div className="jm-nav-a-cont"><a 
         data-testid={`link-a-${links.indexOf(link)}`} 
         className={`jm-nav-a ${theme}-font`} 
         key={link.link} 
         href={link.link}
-        style={{ fontSize: `${fontSize ? fontSize : 1}rem`, borderColor: `jm-turqoise-${theme}mode`}}>
+        style={{ fontSize: `${fontSize ? fontSize : 1}rem`}}>
             {link.title}
-            </a>)}
+            </a>
+            <span className={`jm-nav-a-underscore underscore-${theme}`}/>
+            </div>)}
         </nav>
     </header>
 );
